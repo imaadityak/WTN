f9b314c5f5ad9b34d00aedf685d8bacf42bd3cc9
Bharath1431 <Bharath1431@LAPTOP-6GVAONLG>
2020-06-12 09:28:46 +0530

    command line args 3

---
+++ wipex1/src/wipex1/wipex3.java
@@ -0,0 +1,14 @@
+package wipex1;
+
+public class wipex3 {
+
+	public static void main(String[] args)
+	  {
+		
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c;
+		c=a+b;
+	  System.out.println(c);
+	  }
+}
