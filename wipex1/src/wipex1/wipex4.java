246c9b89e9eb06fec4b73ed18695c9b5627e7caf
Bharath1431 <Bharath1431@LAPTOP-6GVAONLG>
2020-06-12 09:29:19 +0530

    looping ex1

---
+++ wipex1/src/wipex1/wipex4.java
@@ -0,0 +1,23 @@
+package wipex1;
+
+public class wipex4 {
+
+	public static void main(String[] args)
+	  {
+        int a=Integer.parseInt(args[0]);
+        if (a<0)
+        {
+          System.out.println("negative number");
+        }
+        else if(a==0)
+        {
+        	System.out.println("zero");
+        }
+        else
+        {
+        	System.out.println("positive number");
+        	
+        }
+        
+	  }
+}
