a7ce90a6bda0552ed8c15fc969890fc58bcc93f0
Bharath1431 <Bharath1431@LAPTOP-6GVAONLG>
2020-06-12 08:22:29 +0530

    practice ex1

---
+++ wipex1/src/wipex1/wiproex1.java
@@ -0,0 +1,10 @@
+package wipex1;
+
+public class wiproex1
+{
+	 public static void main(String args[])
+	  {
+	  System.out.println(args[0] + "technologies");
+	  System.out.println(args[1]);
+	  }
+}
