09f5901afec57a9a50d2301214c78c340f2b2bcf
Bharath1431 <Bharath1431@LAPTOP-6GVAONLG>
2020-06-13 08:44:17 +0530

    looping 11

---
+++ com.wipro.looping/src/com/wipro/looping/wiploop11.java
@@ -0,0 +1,18 @@
+package com.wipro.looping;
+
+public class wiploop11 {
+
+	public static void main(String args[]) {
+		int n =57;
+		int i;
+		for (i = 23; i <= n; i++) 
+		{
+		   if (i % 2 == 0) 
+		   {
+			System.out.print(i + " ");
+		   }
+		}
+	   }
+	
+	
+}
