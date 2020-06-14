6973dae2a39818b80922eeb05ce8cfeb9d742547
Bharath1431 <Bharath1431@LAPTOP-6GVAONLG>
2020-06-14 14:00:02 +0530

    flow control ex1a

---
+++ WIPFLOWCONTROL/src/com/wipro/flowcontrolstatements/wipflowcontrolex1a.java
@@ -0,0 +1,24 @@
+package com.wipro.flowcontrolstatements;
+
+public class wipflowcontrolex1a 
+{
+	public static void main(String[] args)
+	  {
+      int a=Integer.parseInt(args[0]);
+      if (a<0)
+      {
+        System.out.println("negative number");
+      }
+      else if(a==0)
+      {
+      	System.out.println("zero");
+      }
+      else
+      {
+      	System.out.println("positive number");
+      	
+      }
+      
+	  }
+
+}
