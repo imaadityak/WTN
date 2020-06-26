9f7f06765968593e55369a721933a60f25e28082
Bharath1431 <Bharath1431@LAPTOP-6GVAONLG>
2020-06-26 08:13:48 +0530

    inheritance problem 1

--- com.wipro.looping/src/oops/inheritacceex1.java
+++ com.wipro.looping/src/oops/inheritacceex1.java
@@ -1,1 +1,40 @@
 package oops;
+class Animal 
+{
+	void eat()
+	{ 
+		System.out.println("eat"); 
+	} void sleep() 
+	{
+		System.out.println("sleep");
+	}
+}
+class Bird extends Animal
+{
+   void eat() 
+   { 
+	   System.out.println("Override eat");
+	   
+   }
+   void sleep() 
+   {
+	   System.out.println("Override sleep");
+	   }
+   void fly() 
+   {
+	   System.out.println("fly");
+	   }
+   }
+public class inheritacceex1 
+{
+	public static void main(String args[]) 
+	{
+		Animal a = new Animal();
+		Bird b= new Bird();
+		a.eat();
+		a.sleep(); 
+		b.eat();
+		b.sleep(); 
+		b.fly();
+		} 
+	}
