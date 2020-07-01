18a587f0ee08a5670a2c70f892a0b6cba5e046f7
Bharath1431 <Bharath1431@LAPTOP-6GVAONLG>
2020-07-01 23:48:00 +0530

    String/StringBuffer problem no 7

--- com.wipro.looping/src/oops/Removefirstlastchar.java
+++ com.wipro.looping/src/oops/Removefirstlastchar.java
@@ -4,9 +4,8 @@ public class Removefirstlastchar
 {
  public static void main(String[] args)
  {
-	 StringBuffer str=new StringBuffer();
-	 int l=str.length();
-
-	 System.out.println(str.substring(0,l));
+	    String str="aditya";
+		str = str.substring(1,str.length() - 1);
+		System.out.println(str);
  }
 }
