28a7bcc04bd28ded56efa4cce2426742c5dec9f9
Bharath1431 <Bharath1431@LAPTOP-6GVAONLG>
2020-07-01 23:57:57 +0530

    String/StringBuffer problem no 8

---
+++ com.wipro.looping/src/oops/removecharsbesidestar.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class removecharsbesidestar 
+{
+	public static void main(String[] args) {
+		String str = "ab*cd";
+		String[] strs = str.split(".[\\*]+.");
+		
+		StringBuffer sb = new StringBuffer();
+		for (String x : strs)
+			sb.append(x);
+		System.out.println(sb);
+	}
+}
