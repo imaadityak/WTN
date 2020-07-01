22de07b413b638abca625f289351e7bf4019d602
Bharath1431 <Bharath1431@LAPTOP-6GVAONLG>
2020-07-01 21:01:05 +0530

    inheritance 2

---
+++ com.wipro.looping/src/oops/Person.java
@@ -0,0 +1,22 @@
+package oops;
+
+public class Person 
+{
+private String name;
+ public Person(String n)
+ {
+ name=n; 
+ }
+ public void setName(String n) 
+ {
+ name=n; 
+ }
+ public String getName() 
+ {
+ return name;
+ }
+ public String toString()
+ {
+ return name; 
+ }
+ } 
